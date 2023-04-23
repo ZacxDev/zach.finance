c3601744964fedf694708f797126459943fc9804
Zach <hello@zacx.dev>
2023-04-23 11:25:27 -0300

    remove vue logo, update script lang

--- zach.finance/src/App.vue
+++ zach.finance/src/App.vue
@@ -1,9 +1,8 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
   <VC />
 </template>
 
-<script>
+<script lang="ts">
 import VC from './components/VolatilityChart.vue'
 
 export default {
@@ -20,7 +19,6 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
