d958c1035c8ffc6aa797673aae4e4ec324cf3000
Zach <hello@zacx.dev>
2023-04-29 11:49:31 -0300

    add navbar

--- zach.finance/layouts/default.vue
+++ zach.finance/layouts/default.vue
@@ -2,19 +2,28 @@
   <div>
     <Html :lang="head.htmlAttrs?.lang" :dir="head.htmlAttrs?.dir">
 
-    <Head>
-      <Title>{{ title }}</Title>
-      <template v-for="link in head.link" :key="link.id">
-        <Link :id="link.id" :rel="link.rel" :href="link.href" :hreflang="link.hreflang" />
-      </template>
-      <template v-for="meta in head.meta" :key="meta.id">
-        <Meta :id="meta.id" :property="meta.property" :content="meta.content" />
-      </template>
-    </Head>
+      <Head>
+        <Title>{{ title }}</Title>
+        <template v-for="link in head.link" :key="link.id">
+          <Link :id="link.id" :rel="link.rel" :href="link.href" :hreflang="link.hreflang" />
+        </template>
+        <template v-for="meta in head.meta" :key="meta.id">
+          <Meta :id="meta.id" :property="meta.property" :content="meta.content" />
+        </template>
+      </Head>
 
-    <Body>
-      <slot />
-    </Body>
+      <Body>
+        <div v-if="route.path !== '/'" class="flex justify-center">
+          <div class="navbar w-11/12 card max-w-none" style="background-color:#232323;">
+            <div class="navbar-start">
+              <nuxt-link class="navbar-item" to="/">
+                zach.finance
+              </nuxt-link>
+            </div>
+          </div>
+        </div>
+        <slot />
+      </Body>
 
     </Html>
   </div>
@@ -24,6 +33,7 @@
 const { t } = useI18n()
 const title = computed(() => t('siteTitle'))
 const desc = computed(() => t('siteDescription'))
+const route = useRoute()
 
 useHead({
   meta: [
