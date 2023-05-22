cff6793f93b1f10977c5c5f90da8f5822a91594f
Zach <hello@zacx.dev>
2023-05-22 17:54:36 -0500

    use js object instead of json for firebase admin init

--- zach.finance/nuxt.config.ts
+++ zach.finance/nuxt.config.ts
@@ -85,7 +85,11 @@ export default defineNuxtConfig({
       measurementId: 'G-NQ5QSYJ39D'
     },
     admin: {
-      serviceAccount: './finsune-admin-firebase-service-account.json'
+      serviceAccount: {
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        privateKey: (process.env.FIREBASE_PRIVATE_KEY as string).replace(/\\n/g, '\n'),
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL
+      }
     }
   }
 
