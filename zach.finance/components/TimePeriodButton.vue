a006522530cb6b12c2877840ba90e0cce426efdd
Zach <hello@zacx.dev>
2023-07-25 18:07:12 -0500

    better time period interval values

--- zach.finance/components/TimePeriodButton.vue
+++ zach.finance/components/TimePeriodButton.vue
@@ -22,33 +22,33 @@ const props = defineProps({
 })
 
 const periods = [
-  { label: 'YTD', value: 'ytd' },
-  { label: '1D', value: 1, unit: 'days' },
-  { label: '5D', value: 5, unit: 'days' },
-  { label: '1M', value: 1, unit: 'months' },
-  { label: '3M', value: 3, unit: 'months' },
-  { label: '6M', value: 6, unit: 'months' },
-  { label: '1Y', value: 1, unit: 'years' },
-  { label: '5Y', value: 5, unit: 'years' },
-  { label: 'MAX', value: 'max' }
+  { label: 'YTD', value: 'ytd', period: 'w' },
+  { label: '1D', value: 1, unit: 'days', period: 'd' },
+  { label: '5D', value: 5, unit: 'days', period: 'd' },
+  { label: '1M', value: 1, unit: 'months', period: 'd' },
+  { label: '3M', value: 3, unit: 'months', period: 'w' },
+  { label: '6M', value: 6, unit: 'months', period: 'w' },
+  { label: '1Y', value: 1, unit: 'years', period: 'w' },
+  { label: '5Y', value: 5, unit: 'years', period: 'm' },
+  { label: 'MAX', value: 'max', period: 'm' }
 ]
 
 function selectPeriod(period) {
   if (period.value === 'ytd') {
     const date = new Date()
     const ytd = new Date(date.getFullYear(), 0, 1)
-    emitChange('YTD', 'w', ytd)
+    emitChange('YTD', period.period, ytd)
     return
   }
 
   if (period.value === 'max') {
     const max = new Date(1970, 0, 1)
-    emitChange('Lifetime', '3m', max)
+    emitChange('Lifetime', period.period, max)
     return
   }
 
   const date = sub(new Date(), { [period.unit]: period.value })
-  emitChange(`${period.value} ${period.unit}`, 'd', date)
+  emitChange(`${period.value} ${period.unit}`, period.period, date)
 }
 
 function emitChange(prettyLabel, labelValue, date) {
